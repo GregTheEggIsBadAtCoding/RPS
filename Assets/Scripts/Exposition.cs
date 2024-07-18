30ccc1d3acfd5247b893bfe8049ccf87c20c13cd
GregSherr <gregsherris@gmail.com>
2024-07-18 19:11:47 -0400

    exposition
    y

--- Assets/Scripts/Exposition.cs
+++ Assets/Scripts/Exposition.cs
@@ -6,21 +6,23 @@ using UnityEngine.UI;
 public class Exposition : MonoBehaviour
 {
     [SerializeField] Text text;
+    [SerializeField] GameObject RPS;
     int progress = 0;
     void Start()
     {
         text.text = "After Chronobridge’s rise in biotechnology, many gangs rose to power with enhanced abilities and weaponry. They profited off of fighting rings and whenever a gang member came across someone from another gang, an all-out gang war would start. Violence was at an all-time high, and it seemed there would be no end to it.";
     }
-    void expoNext(){
+    public void expoNext(){
         string result = progress switch{
             0 => "After ten years of non-stop fighting, a new group rose to power called the Cybernetic Collective. They swiftly took control over Chronobridge and imposed new laws and new ways of enforcing them. This included the ban of any civilian fighting whatsoever. With no way to make a profit, and therefore losing much of their power, gangs turned to the only other way they knew how to establish power over one another:",
             1 => "A classic game of...",
-            2 => "Trigger"
+            2 => null
         } ;
-        if (result != "Trigger"){
+        if (result != null){
             progress++;
             text.text = result;
         } else {
+            RPS.SetActive(true);
         }
     }
     void Update()
