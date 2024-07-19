8e1f132ce9251eeb8e654a6193aa27f1c38ae404
PlotYeast <lyddagiraldo@gmail.com>
2024-07-19 12:44:19 -0400

    lowered Rock attack volume

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -87,7 +87,7 @@ public class Interact : MonoBehaviour
                 {
                     //Player Rock beats enemy Scissors
                     case (1, 3):
-                        AudioSource.PlayClipAtPoint(rockAttack, Camera.main.transform.position);
+                        AudioSource.PlayClipAtPoint(rockAttack, Camera.main.transform.position, 0.85f);
                         enemy.GetComponent<SpriteRenderer>().sprite = enemy.enemySprites[3];
                         
                         break;
@@ -123,7 +123,7 @@ public class Interact : MonoBehaviour
                     case (3,1):
                         //enemy Rock beats player Scissors
                         enemy.GetComponent<SpriteRenderer>().sprite = enemy.enemySprites[11];
-                        AudioSource.PlayClipAtPoint(rockAttack, Camera.main.transform.position);
+                        AudioSource.PlayClipAtPoint(rockAttack, Camera.main.transform.position, 0.85f);
                         spriteRenderer.sprite = spriteSheet[2];
                         break;
                 }
